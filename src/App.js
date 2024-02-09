59e9cab87bd43295dc36d0a3e284a37d9487a2d2
AmirAshrafKhan <128276797+AmirAshrafKhan@users.noreply.github.com>
2024-02-09 14:26:36 +0530

    added handleadd and handleremove

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodoList([...todoList, newTodo.trim()]);
+      setNewTodo("");
+    }
+  };
+
+  const handleRemoveTodo = (index) => {
+    const filteredList = todoList.filter((_, i) => i !== index);
+    setTodoList(filteredList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <h3>My ToDo List</h3>
+        <div>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          ></input>
+          <br />
+          <button onClick={handleAddTodo}>Add</button>
+        </div>
+        <ul>
+          {todoList.map((todo, index) => (
+            <li key={index}>
+              {todo}
+              <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
 export default App;
